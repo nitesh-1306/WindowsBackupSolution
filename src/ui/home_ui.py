c9ddcf7d9e460760f42d83b68ab832268bf1804b
Nitesh <niteshmali132002@gmail.com>
2024-12-07 22:57:48 +0530

    Added more cofiguration options regarding the application for the user

--- src/ui/home_ui.py
+++ src/ui/home_ui.py
@@ -26,6 +26,18 @@ class CloudBackupApp(customtkinter.CTk):
             light_image=Image.open(os.path.join(image_path, "settings_dark.png")),
             dark_image=Image.open(os.path.join(image_path, "settings_light.png")), size=(20, 20)
         )
+        self.cloud_image = customtkinter.CTkImage(
+            light_image=Image.open(os.path.join(image_path, "cloud_dark.png")),
+            dark_image=Image.open(os.path.join(image_path, "cloud_light.png")), size=(20, 20)
+        )
+        self.multi_cloud_image = customtkinter.CTkImage(
+            light_image=Image.open(os.path.join(image_path, "multi_cloud_dark.png")),
+            dark_image=Image.open(os.path.join(image_path, "multi_cloud_light.png")), size=(20, 20)
+        )
+        self.folder_settings_image = customtkinter.CTkImage(
+            light_image=Image.open(os.path.join(image_path, "folder_settings_dark.png")),
+            dark_image=Image.open(os.path.join(image_path, "folder_settings_light.png")), size=(28, 28)
+        )
 
         self.generate_navigations_section()
         self.generate_home_page()
@@ -68,14 +80,14 @@ class CloudBackupApp(customtkinter.CTk):
             self.navigation_frame,
             corner_radius=0,
             height=40,
-            border_spacing=10,
-            text="Configurations",
+            border_spacing=6,
+            text=" Folder Settings",
             fg_color="transparent",
             text_color=("gray10", "gray90"),
             hover_color=("gray70", "gray30"),
             anchor="w",
             command=self.configurations_button_event,
-            image=self.settings_image
+            image=self.folder_settings_image
         )
         self.configurations_button.grid(row=2, column=0, sticky="ew")
 
@@ -112,16 +124,99 @@ class CloudBackupApp(customtkinter.CTk):
         )
         self.title_label.grid(row=0, column=0, padx=20, pady=20)
 
-        self.switch_var = customtkinter.StringVar(value="on")
-        self.switch = customtkinter.CTkSwitch(
+        self.enable_backup = customtkinter.StringVar(value="on")
+        font = customtkinter.CTkFont(size=16)
+        self.enable_backup_switch = customtkinter.CTkSwitch(
             self.home_frame,
-            text="Auto Backup",
-            command=self.switch_event,
-            variable=self.switch_var,
+            text="Toggle Auto Backup",
+            command=self.enable_backup_switch_event,
+            variable=self.enable_backup,
+            onvalue="on",
+            offvalue="off",
+            font=font
+        )
+        self.enable_backup_switch.grid(row=1, column=0, padx=20, pady=(0, 10), sticky="w")
+
+        self.backup_info = customtkinter.CTkLabel(
+            self.home_frame,
+            text="*Enable it to automatically backup all the files on your PC. These files are stored in multiple cloud platforms at once for safety.",
+            compound="left",
+            anchor="w",
+            font=customtkinter.CTkFont(size=10, weight="normal", slant="italic"),
+            wraplength=450,
+            justify="left"
+        )
+        self.backup_info.grid(row=2, column=0, padx=30, pady=0, sticky="w")
+
+        self.cloud_options_title_label = customtkinter.CTkLabel(
+            self.home_frame,
+            text=" Cloud Options",
+            compound="left",
+            font=customtkinter.CTkFont(size=20, weight="bold")
+        )
+        self.cloud_options_title_label.grid(row=3, column=0, padx=20, pady=(20,0))
+
+        self.cloud_options_label = customtkinter.CTkLabel(
+            self.home_frame,
+            text=" Select Priority Cloud:",
+            compound="left",
+            font=customtkinter.CTkFont(size=16, weight="bold"),
+            image=self.cloud_image
+        )
+        self.cloud_options_label.grid(row=4, column=0, padx=20, pady=0, sticky="w")
+
+        self.cloud_options = customtkinter.CTkComboBox(
+            self.home_frame, 
+            values=["Telegram Cloud", "Google Drive", "Mediafire", "Catbox"],
+            command=self.dropdown_callback,
+            corner_radius=7,
+            state="readonly"
+        )
+        self.cloud_options.grid(row=5, column=0, padx=20, pady=10, sticky="w")
+        self.cloud_options.set("Select an option")
+
+        self.cloud_warning = customtkinter.CTkLabel(
+            self.home_frame,
+            text="*Every cloud service has some advantages and disadvantages, you have the choice to choose appropriate.",
+            compound="left",
+            anchor="w",
+            font=customtkinter.CTkFont(size=10, weight="normal", slant="italic"),
+            wraplength=450,
+            justify="left"
+        )
+        self.cloud_warning.grid(row=6, column=0, padx=30, pady=0,sticky="w")
+
+
+        self.multi_cloud_upload_label = customtkinter.CTkLabel(
+            self.home_frame,
+            text=" Enable Multi Cloud Upload:",
+            compound="left",
+            font=customtkinter.CTkFont(size=16, weight="bold"),
+            image=self.multi_cloud_image
+        )
+        self.multi_cloud_upload_label.grid(row=7, column=0, padx=20, pady=(25, 0), sticky="w")
+
+        self.multi_cloud_description = customtkinter.CTkLabel(
+            self.home_frame,
+            text="*Enabling this option will backup your files on multiple clouds, but will consume more internet, data will be backed up on the priority cloud first.",
+            compound="left",
+            anchor="w",
+            font=customtkinter.CTkFont(size=10, weight="normal", slant="italic"),
+            wraplength=450,
+            justify="left"
+        )
+        self.multi_cloud_description.grid(row=8, column=0, padx=30, pady=0,sticky="w")
+
+        self.multi_cloud_upload = customtkinter.StringVar(value="off")
+        self.multi_cloud_upload_switch = customtkinter.CTkSwitch(
+            self.home_frame,
+            text="Multi Cloud Upload",
+            command=self.enable_multi_cloud_upload_event,
+            variable=self.multi_cloud_upload,
             onvalue="on",
             offvalue="off"
         )
-        self.switch.grid(row=1, column=0, padx=20, pady=10, sticky="w")
+        self.multi_cloud_upload_switch.grid(row=9, column=0, padx=20, pady=10, sticky="w")
 
     def generate_configurations_page(self):
         self.configurations_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -148,7 +243,7 @@ class CloudBackupApp(customtkinter.CTk):
             self.configurations_frame,
             width=250,
             height=100,
-            corner_radius=5,
+            corner_radius=7,
             activate_scrollbars=False
         )
         self.folders_ignore_list.grid(row=2, column=0, padx=(20, 0), pady=(20, 0), sticky="w")
@@ -162,7 +257,7 @@ class CloudBackupApp(customtkinter.CTk):
             self.configurations_frame,
             placeholder_text="Enter folder names to ignore...",
             width=250,
-            corner_radius=5
+            corner_radius=7
         )
         self.folder_ignore_entry.grid(row=3, column=0, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="w")
         self.folder_ignore_entry.bind("<Return>", self.add_to_ignore_list)
@@ -171,7 +266,7 @@ class CloudBackupApp(customtkinter.CTk):
             self.configurations_frame,
             text="Add",
             command=self.add_to_ignore_list,
-            corner_radius=5
+            corner_radius=7
         )
         self.ignore_list_add_button.grid(row=3, column=1, padx=(0, 75), pady=(20, 0), sticky="w")
     
@@ -237,8 +332,21 @@ class CloudBackupApp(customtkinter.CTk):
                     self.folders_ignore_list.insert("0.0", f"{folder}\n")
                 self.folders_ignore_list.configure(state="disabled")
 
-    def switch_event(self):
-        print("switch toggled, current value:", self.switch_var.get())
+    def enable_backup_switch_event(self):
+        print("Switch toggled, current value:", self.enable_backup.get())
+    
+    def enable_multi_cloud_upload_event(self):
+        print("Switch toggled, current value:", self.multi_cloud_upload.get())
+
+    def dropdown_callback(self, choice):
+        warnings = {
+            "Telegram Cloud": "*This service might shutdown due to legal restrictions, but it has unlimited storage capability.",
+            "Google Drive": "*This service is a very reliable option, but it has a low storage of only 20 GB.",
+            "Mediafire": "*This service is reliable, but has a low 'max file size' and low storage of 10GB.",
+            "Catbox": "*This service is has unlimited storage capacity, but a low 'max file size' and files might not be preserved."
+        }
+        self.cloud_warning.configure(text=warnings[choice])
+        print(f"Selected: {choice}")
     
 
 
